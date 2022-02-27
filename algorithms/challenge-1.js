37d63fddf4304ea8dc686e030b61b3c5fd7283bf
Uozyoma <78306547+Uozyoma@users.noreply.github.com>
2022-02-27 15:23:00 +0100

    Update challenge-1.js
    Challenge 1 completed

--- algorithms/challenge-1.js
+++ algorithms/challenge-1.js
@@ -1,3 +1,22 @@
-function noOfElements(x, y, z) {}
+
+function noOfElements(x=[],y ,z ) {
+var nomber = 0;
+var count = 0;
+
+for ( let O of  x) {
+
+   if ((O >= y) && ( O <= z)) {
+count++;
+   }
+nomber =count;
+
+}
+console.log(nomber);
+ }
+ noOfElements([2,3,0,1,7], 4, 8) ;
+noOfElements([3,3,3,5,18,6,20], 15, 30);
+noOfElements([4,6,2,6,7,7], 0, 7);
+noOfElements([4,6,2,6,7,7, 15, 18, 27], 5, 28);
+
 
 module.exports = noOfElements
